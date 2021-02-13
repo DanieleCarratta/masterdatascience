89baf5e373377811fb8ecf775bc3a798d28c7727
Antonio Caruso <antonio.mario.caruso@gmail.com>
2021-02-13 16:01:40 +0100

    Add files via upload

---
+++ film.py
@@ -0,0 +1,79 @@
+
+movies = [
+ 'Il Miglio Verde',
+ 'Il Piccolo Gigante Gentile',
+ 'Seven',
+ 'Avatar',
+ 'Il Piccolo Principe',
+ 'La Vita è Bella',
+ 'Matrix',
+ 'Forest Gump',
+ 'The Truman Show',
+ 'Ritorno al Futuro',
+ 'Harry Potter'
+]
+
+preferenze = { 'Caruso':
+ {
+ 'Il Miglio Verde':             7.8, 
+ 'Il Piccolo Gigante Gentile':  6.5,
+ 'Seven':                       0,
+ 'Avatar':                      7.5,
+ 'Il Piccolo Principe':         4.3,
+ 'La Vita è Bella':             7,
+ 'Matrix':                      10,
+ 'Forest Gump':                 0,
+ 'The Truman Show':             5.4,
+ 'Ritorno al Futuro':           5,
+ 'Harry Potter':                0
+ },
+ 'Blasi': {
+ 'Il Miglio Verde':             8, 
+ 'Il Piccolo Gigante Gentile':  0,
+ 'Seven':                       7,
+ 'Avatar':                      9.5,
+ 'Il Piccolo Principe':         0,
+ 'La Vita è Bella':             0,
+ 'Matrix':                      8,
+ 'Forest Gump':                 5,
+ 'The Truman Show':             7,
+ 'Ritorno al Futuro':           6,
+ 'Harry Potter':                7
+ },
+ 'Marino':
+ {
+     'Il Miglio Verde':             10  , 
+     'Il Piccolo Gigante Gentile':  8.5 ,
+     'Seven':                       0   ,
+     'Avatar':                      8   ,
+     'Il Piccolo Principe':         7.9 ,
+     'La Vita è Bella':             10  ,
+     'Matrix':                      10  ,
+     'Forest Gump':                 10  ,
+     'The Truman Show':             9.5 ,
+     'Ritorno al Futuro':           8.9 ,
+     'Harry Potter':                0
+ },
+ 'Primo': {
+     'Il Miglio Verde':           8   ,
+     'Il Piccolo Gigante Gentile':10  ,
+     'Seven':                     8   ,
+     'Avatar':                    7   ,
+     'Il Piccolo Principe':       10  ,
+     'La Vita è Bella':           10  ,
+     'Matrix':                    9   ,
+     'Forest Gump':               8   ,
+     'The Truman Show':           8   ,
+     'Ritorno al Futuro':         8   ,
+     'Harry Potter':              8
+     
+ },
+ 'Distante': {},
+ 'Gravili': {},
+ 'Manca': {},
+ 'Tommasi': {},
+ 'Melle': {},
+ 'Carratta': {}
+}
+ 
+ 
